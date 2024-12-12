1628e17b330fb55146e61de703ee13c7e58c831a
20210699 <20210699@duksung.ac.kr>
2024-12-12 17:32:31 +0900

    ✨ Feat: 결제 취소 api 구현

--- src/main/java/com/telegro/telegro/domain/payment/controller/PaymentController.java
+++ src/main/java/com/telegro/telegro/domain/payment/controller/PaymentController.java
@@ -2,12 +2,16 @@ package com.telegro.telegro.domain.payment.controller;
 
 import com.siot.IamportRestClient.IamportClient;
 import com.siot.IamportRestClient.exception.IamportResponseException;
+import com.siot.IamportRestClient.request.CancelData;
 import com.siot.IamportRestClient.response.IamportResponse;
 import com.siot.IamportRestClient.response.Payment;
 import com.telegro.telegro.domain.cart.entity.Cart;
 import com.telegro.telegro.domain.cart.repository.CartRepository;
+import com.telegro.telegro.domain.order.entity.Order;
+import com.telegro.telegro.domain.order.repository.OrderRepository;
 import com.telegro.telegro.domain.payment.dto.request.PaymentRequestDTO;
 import com.telegro.telegro.domain.payment.service.PaymentService;
+import com.telegro.telegro.domain.user.entity.User;
 import com.telegro.telegro.global.apiPayLoad.exception.CustomException;
 import com.telegro.telegro.global.apiPayLoad.exception.Error;
 import jakarta.annotation.PostConstruct;
@@ -24,10 +28,12 @@ import java.util.List;
 @RequestMapping("api/v1")
 @RequiredArgsConstructor
 @Slf4j
+//Todo : url 통일성 있게 수정
 public class PaymentController implements PaymentControllerDocs{
 
     private final HttpSession httpSession;
     private final CartRepository cartRepository;
+    private final OrderRepository orderRepository;
     private IamportClient iamportClient;
     private final PaymentService paymentService;
 
@@ -50,6 +56,23 @@ public class PaymentController implements PaymentControllerDocs{
         log.info("결제 요청 응답. 결제 내역 - 주문 번호: {}", payment.getResponse().getMerchantUid());
 
         paymentService.processPaymentDone(id, request, imp_uid);
+
+        return payment;
+    }
+
+    @PostMapping("/{orderId}")
+    public IamportResponse<Payment> cancelPayment(Long id, Long orderId) throws IamportResponseException, IOException {
+
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> CustomException.of(Error.ORDER_NOT_FOUND));
+
+        // Todo : 관리자 혹은 주문자만 결제 취소 가능
+
+        CancelData cancelData = new CancelData(order.getOrderNumber(), true);
+
+        IamportResponse<Payment> payment = iamportClient.cancelPaymentByImpUid(cancelData);
+
+        // Todo : 결제 취소 시 주문, 결제 상태 변경(웹훅으로 상태 관리)
 
         return payment;
     }
--- src/main/java/com/telegro/telegro/domain/payment/controller/PaymentControllerDocs.java
+++ src/main/java/com/telegro/telegro/domain/payment/controller/PaymentControllerDocs.java
@@ -12,11 +12,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.io.IOException;
 
-//Todo : Payment 관련 추후 리팩토링
 public interface PaymentControllerDocs {
     @Operation(summary = "결제 정보 저장", description = "결제 정보를 저장한다.")
     @ApiResponse(responseCode = "200", description = "결제 정보 저장 성공")
     public IamportResponse<Payment> validateIamport(@LoginInfo Long id, @PathVariable String imp_uid, @RequestBody PaymentRequestDTO request) throws IamportResponseException, IOException;
+
+    @Operation(summary = "결제 취소", description = "결제를 취소하고, 주문 상태를 변경한다")
+    @ApiResponse(responseCode = "200", description = "결제 취소 성공")
+    public IamportResponse<Payment> cancelPayment(@LoginInfo Long id, @PathVariable Long orderId) throws IamportResponseException, IOException;
 
     @Operation(summary = "주문 정보 삭제", description = "결제 완료 화면에서 세션 저장값, 장바구니 삭제한다.")
     @ApiResponse(responseCode = "200", description = "주문 정보 삭제 성공")
