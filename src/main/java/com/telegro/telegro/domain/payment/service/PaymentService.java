3b7d377b519ce40d121a1943051c041253912b96
20210699 <20210699@duksung.ac.kr>
2024-12-24 23:20:49 +0900

    ✨ Feat: 가상계좌 결제 상태 변경
    자정이 되면 pending -> failed 로 상태 변경

---
+++ src/main/java/com/telegro/telegro/domain/payment/service/PaymentService.java
@@ -0,0 +1,29 @@
+package com.telegro.telegro.domain.payment.service;
+
+import com.telegro.telegro.domain.order.entity.Order;
+import com.telegro.telegro.domain.order.entity.enums.PaymentStatus;
+import com.telegro.telegro.domain.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+    private final OrderRepository orderRepository;
+
+    @Transactional
+    public void updatePendingPaymentsToFailed() {
+        LocalDateTime threshold = LocalDateTime.now().minusDays(1);
+        List<Order> pendingPayments = orderRepository.findByPaymentStatusAndUpdatedAtBefore(PaymentStatus.PENDING, threshold);
+
+        for (Order order : pendingPayments) {
+            order.setPaymentStatus(PaymentStatus.FAILED);
+        }
+
+        orderRepository.saveAll(pendingPayments);
+    }
+}
