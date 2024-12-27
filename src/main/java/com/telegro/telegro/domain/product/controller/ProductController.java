8d8ae97bab029c9c4acb5c433272e6bdfff269b9
20210699 <20210699@duksung.ac.kr>
2024-12-27 15:17:41 +0900

    🐛 Fix: /products 재작성

--- src/main/java/com/telegro/telegro/domain/product/controller/ProductController.java
+++ src/main/java/com/telegro/telegro/domain/product/controller/ProductController.java
@@ -18,7 +18,7 @@ import java.util.Map;
 public class ProductController implements ProductControllerDocs{
     private final ProductService productService;
 
-    @GetMapping
+    @GetMapping("/products")
     public SuccessResponse<ProductListDTO> getProducts(Long id, String query, Category category, int page, int size) {
         return SuccessResponse.of(productService.getProducts(id, category, page, size));
     }
